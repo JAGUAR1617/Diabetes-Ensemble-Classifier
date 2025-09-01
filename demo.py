a4ce7455f3c16b088d8aec2b5cf024c0cc8f8efb
PJha <32899884+JAGUAR1617@users.noreply.github.com>
2025-09-01 15:52:55 +0530

    Update demo.py
    renamed csv file

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ from tensorflow.keras.layers import Dense, Input
 # ---------------------------
 @st.cache_data
 def load_data():
-    df = pd.read_csv("diabetes.csv")  # change to your dataset path
+    df = pd.read_csv("Diabeted_Ensemble.csv")  # change to your dataset path
     return df
 
 df = load_data()
